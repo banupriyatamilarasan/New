c3f6d6bc19c1ce5ba8be28bb297da3ef21c9138f
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 18:47:17 +0530

    Create

---
+++ src/test/java/org/sam/NewTask.java
@@ -0,0 +1,14 @@
+package org.sam;
+
+public class NewTask {
+	private void empName() {
+		System.out.println("vel");
+	}
+	private void empName1() {
+		System.out.println("mahi");
+	}
+	private void empName2() {
+		System.out.println("ram");
+	}
+
+}
